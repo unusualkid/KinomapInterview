a6a711c00d62f7974809cda3d79787069a17067c
Kenneth Chen <unusualkid@gmail.com>
2019-03-08 12:10:27 +0100

    First commit.

--- KinomapInterview/ViewController.swift
+++ KinomapInterview/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import AlamoFire
 
 class ViewController: UIViewController {
 
+    var vehicles: [[String: Any]] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
 }
 
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellReuseIdentifier = "TableViewCell"
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as! TableViewCell
+        
+        return cell
+    }
+}
